d14bda3cc2fec3d64ce018ef462e32722dbbb4e2
Frederic Jacob <frederic.jacob.78@gmail.com>
2018-04-10 21:30:28 -0400

    Add the home page

--- frontend/modules/home.js
+++ frontend/modules/home.js
@@ -1,14 +1,61 @@
 import React from 'react'
+import jquery from 'jquery'
+import { Table, NavLink } from 'reactstrap';
+import createHistory from "history/createHashHistory"
+
+const history = createHistory();
 
 class Home extends React.Component{
         constructor(props) {
                 super(props);
+                this.state = {events: [], invalid: true};
+                jquery.ajax({
+                type: 'GET',
+                url: "/mididec/api/v1.0/events",
+                success: this.success.bind(this),
+                error: this.error.bind(this),
+                contentType: "application/json",
+                dataType: 'json'
+                });
+        }
+
+        success(data){
+                this.state.events = data.events;
+                this.state.invalid = false;
+                this.setState(this.state);
+        }
+
+        error(){
+
+        }
+
+        onEventDetails(uid){
+                console.log(uid);
+                history.push("/events/"+uid);
         }
 
         render(){
+                const listItems = this.state.events.map((event) =>
+                        <tr>
+                          <th>{event.title}</th>
+                          <td>{event.start}</td>
+                          <td><div className='event-detail-link' onClick={(e) => this.onEventDetails(event.uid)}>Détails</div></td>
+                        </tr>
+                  );
                 return (
                         <div className='home'>
-                                <div> Home </div>
+                                <Table>
+                                        <thead>
+                                          <tr>
+                                            <th>Titre</th>
+                                            <th>Date</th>
+                                            <th></th>
+                                          </tr>
+                                        </thead>
+                                        <tbody>
+                                          {listItems}
+                                        </tbody>
+                                </Table>
                         </div>)
         }
 }
