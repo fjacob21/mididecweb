03a6251f9c870a927d92ca3855296c89aefb41d3
Frederic Jacob <frederic.jacob.78@gmail.com>
2018-04-11 18:40:11 -0400

    Set Date in UTC

--- frontend/modules/dateformater.js
+++ frontend/modules/dateformater.js
@@ -13,12 +13,12 @@ class DateFormater {
   }
 
   getDateText(){
-    var dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', timeZone: 'UTC'};
     return this._date.toLocaleDateString('fr-CA', dateOptions);
   }
 
   getTimeText(){
-    var timeOptions = { hour: 'numeric', minute: 'numeric', second: undefined};
+    var timeOptions = { hour: 'numeric', minute: 'numeric', second: undefined, timeZone: 'UTC'};
     return this._date.toLocaleTimeString('fr-CA', timeOptions).replace(/ /g, '');
   }
 }
--- frontend/modules/tests/dateformater.test.js
+++ frontend/modules/tests/dateformater.test.js
@@ -3,7 +3,7 @@ import DateFormater from '../dateformater';
 import renderer from 'react-test-renderer';
 
 test('A class to format a Date', () => {
-  var dateStr = '2018-04-05T23:20:19Z';
+  var dateStr = '2018-04-05T23:20:19+00:00';
   var date = new Date(dateStr);
   var dateform1 = new DateFormater(dateStr);
   var dateform2 = new DateFormater(date);
@@ -11,9 +11,9 @@ test('A class to format a Date', () => {
   expect(dateform1.getDate().constructor.name).toBe('Date');
   expect(typeof dateform2.getDate()).toBe('object');
   expect(dateform2.getDate().constructor.name).toBe('Date');
-  expect(dateform1.getTimeText()).toBe('18h20');
+  expect(dateform1.getTimeText()).toBe('23h20');
   expect(dateform1.getDateText()).toBe('jeudi 5 avril 2018');
-  expect(dateform2.getTimeText()).toBe('18h20');
+  expect(dateform2.getTimeText()).toBe('23h20');
   expect(dateform2.getDateText()).toBe('jeudi 5 avril 2018');
 
 });
