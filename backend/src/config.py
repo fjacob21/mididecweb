eeb96c2fe2c69f840700697659592852348f667d
Frederic Jacob <fjacob21@users.noreply.github.com>
2018-05-14 21:28:54 -0400

    Change way of loading configs (#82)

--- backend/src/config.py
+++ backend/src/config.py
@@ -5,42 +5,45 @@ import os
 class Config(object):
 
     def __init__(self):
-        data = {}
+        self._path = self.find_config_file()
+
+    def find_config_file(self):
         if os.path.exists('/etc/mididec/config.json'):
-            with open('/etc/mididec/config.json') as f:
-                data = json.load(f)
+            return '/etc/mididec/config.json'
         elif os.path.exists('../data/config.json'):
-            with open('../data/config.json') as f:
-                data = json.load(f)
-        else:
-            with open('config.json') as f:
-                data = json.load(f)
-        self._data = data
+            return '../data/config.json'
+        return 'config.json'
+
+    def load_config_file(self):
+        data = {}
+        with open(self._path) as f:
+            data = json.load(f)
+        return data
 
     @property
     def root(self):
-        return self._data['root']
+        return self.load_config_file()['root']
 
     @property
     def database(self):
-        return self._data['database']['path']
+        return self.load_config_file()['database']['path']
 
     @property
     def sms_sid(self):
-        return self._data['sms']['sid']
+        return self.load_config_file()['sms']['sid']
 
     @property
     def sms_token(self):
-        return self._data['sms']['token']
+        return self.load_config_file()['sms']['token']
 
     @property
     def email_server(self):
-        return self._data['email']['server']
+        return self.load_config_file()['email']['server']
 
     @property
     def email_user(self):
-        return self._data['email']['user']
+        return self.load_config_file()['email']['user']
 
     @property
     def email_password(self):
-        return self._data['email']['password']
+        return self.load_config_file()['email']['password']
