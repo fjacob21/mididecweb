45609c8b5ed9fb762d4bff2e4fbf8a068e2ebdd0
Frederic Jacob <frederic.jacob.78@gmail.com>
2018-05-18 22:03:22 -0400

    Do a better design of the event summary display

--- frontend/modules/eventsummary.js
+++ frontend/modules/eventsummary.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import createHistory from "history/createHashHistory"
 import DateFormater from './dateformater'
+import { Table, NavLink, Card, CardTitle, CardText, Button } from 'reactstrap';
 
 const history = createHistory();
 
@@ -10,7 +11,6 @@ class EventSummary extends React.Component {
         }
 
         onEventDetails(event_id, isItem){
-            if (!isItem || window.innerWidth < 500)
                 history.push("/events/"+event_id);
         }
 
@@ -18,11 +18,15 @@ class EventSummary extends React.Component {
           var df = new DateFormater(this.props.event.start);
           var start = df.getDateText();
           var time = df.getTimeText();
+          var attendeesNumber = this.props.event.attendees.length;
+          var attendeesLeft = this.props.event.max_attendee - attendeesNumber;
           return (
                   <div className='event-summary' onClick={(e) => this.onEventDetails(this.props.event.event_id, true)}>
-                      <div className='es-title'>{this.props.event.title}</div>
-                      <div className='es-start-long'>{start + ' ' + time}</div>
-                      <div className='es-detail'><div className='event-detail-link' onClick={(e) => this.onEventDetails(this.props.event.event_id, false)}>Détails</div></div>
+                        <Card body className='home-card'>
+                                <CardTitle><div className='es-title'>{this.props.event.title}</div></CardTitle>
+                                <div className='es-start-long'>{start + ' ' + time}</div>
+                                <div className='es-attendees'>{attendeesNumber} personnes inscrites - <font color="red">{attendeesLeft} places disponibles</font></div>
+                        </Card>
                   </div>)
         }
 }
