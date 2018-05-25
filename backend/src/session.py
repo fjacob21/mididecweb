0a6f820387f6c27c600c0badc4dc5f458b0ad5aa
Frederic Jacob <fjacob21@users.noreply.github.com>
2018-05-24 22:30:29 -0400

    Coolemails (#136)
    Modify email to be better looking

--- backend/src/session.py
+++ backend/src/session.py
@@ -14,6 +14,7 @@ from event import ATTENDEE_LIST, WAITING_LIST
 from jinja2 import Environment, FileSystemLoader
 from session_exception import SessionError
 import errors
+import locale
 
 
 class Session(object):
@@ -62,6 +63,25 @@ class Session(object):
         if not event:
             raise SessionError(errors.ERROR_INVALID_EVENT)
         return iCalGenerator(event).generate()
+
+    def get_event_jinja(self, event_id):
+        event = self._events.get(event_id)
+        if not event:
+            raise SessionError(errors.ERROR_INVALID_EVENT)
+        env = Environment(loader=FileSystemLoader('emails'))
+        t = env.get_template('waitingconfirm.html')
+
+        event_obj = self.generate_email_event(event)
+        html = t.render(user=self.user, event=event_obj, server=self._server)
+        sender = EmailSender(self._config.email_user,
+                             self._config.email_password,
+                             'fjacob21@hotmail.com',
+                             'Confirmation MidiDecouverte',
+                             html,
+                             'html',
+                             self._config.email_server)
+        sender.send()
+        return html
 
     def add_event(self):
         if "title" not in self._params or "description" not in self._params:
@@ -154,6 +174,7 @@ class Session(object):
         if (self._config and self._config.email_user and
            self._config.email_password and self._config.email_server):
             env = Environment(loader=FileSystemLoader('emails'))
+            event_obj = self.generate_email_event(event)
             if list == ATTENDEE_LIST:
                 t = env.get_template('registerconfirm.html')
             else:
@@ -163,7 +184,7 @@ class Session(object):
                                  user.email,
                                  'Confirmation MidiDecouverte',
                                  t.render(user=user,
-                                          event=event,
+                                          event=event_obj,
                                           server=self._server),
                                  'html',
                                  self._config.email_server)
@@ -188,6 +209,7 @@ class Session(object):
     def send_promotee_email(self, event, promotee):
         if (self._config and self._config.email_user and
            self._config.email_password and self._config.email_server):
+            event_obj = self.generate_email_event(event)
             env = Environment(loader=FileSystemLoader('emails'))
             t = env.get_template('promotee.html')
             sender = EmailSender(self._config.email_user,
@@ -195,7 +217,7 @@ class Session(object):
                                  promotee.email,
                                  'Confirmation MidiDecouverte',
                                  t.render(user=promotee,
-                                          event=event,
+                                          event=event_obj,
                                           server=self._server),
                                  'html',
                                  self._config.email_server)
@@ -218,6 +240,7 @@ class Session(object):
            self._config.email_password and self._config.email_server):
             env = Environment(loader=FileSystemLoader('emails'))
             t = env.get_template('eventpublish.html')
+            event_obj = self.generate_email_event(event)
             res = True
             for user in self._users.list:
                 if user.useemail and user.email and user.validated:
@@ -226,7 +249,7 @@ class Session(object):
                                          user.email,
                                          'Confirmation MidiDecouverte',
                                          t.render(user=user,
-                                                  event=event,
+                                                  event=event_obj,
                                                   server=self._server),
                                          'html',
                                          self._config.email_server)
@@ -418,3 +441,20 @@ class Session(object):
         if not user.logout(loginkey):
             raise SessionError(errors.ERROR_INVALID_REQUEST)
         return {'result': True}
+
+    def generate_email_event(self, event):
+        event_obj = {}
+        event_obj['title'] = event.title
+        try:
+            locale.setlocale(locale.LC_ALL, 'fr_CA')
+        except Exception:
+            pass
+        start = datetime.strptime(event.start, "%Y-%m-%dT%H:%M:%SZ")
+        end = datetime.strptime(event.end, "%Y-%m-%dT%H:%M:%SZ")
+        event_obj['day'] = start.strftime("%A")
+        event_obj['start'] = start.strftime("%d %B %Y")
+        event_obj['times'] = start.strftime("%H:%M %Z") + ' - ' + end.strftime("%H:%M %Z")
+        event_obj['description'] = event.description
+        event_obj['location'] = event.location
+        event_obj['organizer_name'] = event.organizer_name
+        return event_obj
