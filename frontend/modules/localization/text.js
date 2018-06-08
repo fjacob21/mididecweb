80abddc48637cbeb2fa825f4bcc9894e9cf0738e
Frederic Jacob <frederic.jacob.78@gmail.com>
2018-06-08 18:57:22 -0400

    Add more language using Google translate

--- frontend/modules/localization/text.js
+++ frontend/modules/localization/text.js
@@ -1,5 +1,11 @@
 import frca from './fr-CA.json'
 import enca from './en-CA.json'
+import it from './it.json'
+import ru from './ru.json'
+import ja from './ja.json'
+import ar from './ar.json'
+import de from './de.json'
+import es from './es.json'
 
 class Text {
         static get text(){
@@ -31,5 +37,11 @@ class Text {
 Text.locales = []
 Text.locales['fr-CA'] = frca
 Text.locales['en-CA'] = enca
+Text.locales['it'] = it
+Text.locales['ru'] = ru
+Text.locales['ja'] = ja
+Text.locales['ar'] = ar
+Text.locales['de'] = de
+Text.locales['es'] = es
 Text.obj = null;
 module.exports = Text;
