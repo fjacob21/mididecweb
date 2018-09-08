89988f57de3a2a7ba36998b32a74809dd66db55b
Frederic Jacob <fjacob21@users.noreply.github.com>
2018-09-08 00:22:14 -0400

    Display full name when logged with root or manager access (#237)

--- frontend/modules/attendeeicon.js
+++ frontend/modules/attendeeicon.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import User from './user'
 
 class AttendeeIcon extends React.Component{
 
@@ -7,12 +8,16 @@ class AttendeeIcon extends React.Component{
         }
 
         render(){
+                var user = User.getSession();
                 var avatar = <i className="material-icons md-light attendee-avatar">account_circle</i>
                 if (this.props.attendee.have_avatar) {
                         var avatar_path = "/mididec/api/v1.0/users/" + this.props.attendee.user_id+"/avatar?sizex=100&sizey=110&id=" + new Date().getTime();
                         avatar = <img src={avatar_path} className="attendee-avatar"/>
                 }
                 var alias = this.props.attendee.alias;
+                if (user && (user.isManager || user.isSuperUser)) {
+                  alias = this.props.attendee.name;
+                }
                 if (this.props.noname)
                         alias = "";
                 var className = this.props.className + " attendeeicon " ;
