162a4a8133c907fd5f6f4ec56ec23a85e5f23289
Frederic Jacob <frederic.jacob.78@gmail.com>
2018-09-11 21:05:15 -0400

    Add the number of attendees in the event page

--- frontend/modules/eventbig.js
+++ frontend/modules/eventbig.js
@@ -30,25 +30,35 @@ class EventBig extends React.Component{
                 var timeText = this._start.getTimeText() + ' à ';
                 timeText += this._end.getTimeText();
                 var icalurl = '/mididec/api/v1.0/events/' + this.props.event.event_id + '/ical';
-                var attendees = "";
-                var attachments = "";
                 var linkedText = Autolinker.link( this.props.event.description );
                 linkedText = {__html: linkedText};
-                if (user)
-                    attendees = this.props.event.attendees.map((attendee) =>
+                var attendees = "";
+                if (user && this.props.event.attendees.length) {
+                    var attendeeslist = this.props.event.attendees.map((attendee) =>
                             <AttendeeIcon key={attendee.user_id} attendee={attendee} className='attendee-icon'/>
                     );
+                    attendees = (<div><div className='attendees-title'>{Text.text.event_attendees_label}({this.props.event.attendees.length})</div>
+                    <div className='attendeesgrid'>
+                            {attendeeslist}
+                    </div></div>);
+                }
                 var eventurl = '/mididec/api/v1.0/events/' + this.props.event.event_id + "/attachments/";
-                if (user)
-                    attachments = this.props.event.attachments.map((attachment) =>
+                var attachments = "";
+                if (user && this.props.event.attachments.length){
+                    var attachmentslist = this.props.event.attachments.map((attachment) =>
                             <a key={attachment} href={eventurl+attachment} className='attachment-item'>{attachment}</a>
                     );
+                    attachments = (<div><div className='attachments-title'>{Text.text.event_attachments_label}</div>
+                    <div className='attachmentsgrid'>
+                            {attachmentslist}
+                    </div></div>);
+                }
                 var waintings = '';
-                if (user && (user.isManager || user.isSuperUser)) {
+                if (user && (user.isManager || user.isSuperUser) && this.props.event.waitings.length) {
                   var waitingslist = this.props.event.waitings.map((waiting) =>
                           <AttendeeIcon key={waiting.user_id} attendee={waiting} className='attendee-icon'/>
                   );
-                  waintings = (<div><div className='attendees-title'>{Text.text.event_waitings_label}</div>
+                  waintings = (<div><div className='attendees-title'>{Text.text.event_waitings_label}({this.props.event.waitings.length})</div>
                   <div className='attendeesgrid'>
                           {waitingslist}
                   </div></div>);
@@ -74,15 +84,9 @@ class EventBig extends React.Component{
                                         <div className='detail'>
                                                 <div className='detaillabel'> {Text.text.event_details_label} </div>
                                                 <div className='description' dangerouslySetInnerHTML={linkedText}/>
-                                                <div className='attendees-title'>{Text.text.event_attendees_label}</div>
-                                                <div className='attendeesgrid'>
-                                                        {attendees}
-                                                </div>
+                                                {attendees}
                                                 {waintings}
-                                                <div className='attachments-title'>{Text.text.event_attachments_label}</div>
-                                                <div className='attachmentsgrid'>
-                                                        {attachments}
-                                                </div>
+                                                {attachments}
                                         </div>
                                         <div className='info'>
                                                 <div className='duration'>
